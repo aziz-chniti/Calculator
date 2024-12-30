f34b6e3695b809bdca5fd69b9c3aba9b272ad22b
aziz-chniti <azizchniti0@gmail.com>
2024-12-30 12:08:08 +0100

    initial commit

---
+++ assets/main.js
@@ -0,0 +1,32 @@
+const input = document.getElementById('inp');
+
+const btns = document.querySelectorAll('button');
+
+let string = ""
+
+btns.forEach(btn =>{
+    btn.addEventListener("click" , ()=>{
+        if (btn.innerHTML == "AC") {
+            string = ""
+            input.value = string
+        }else if(btn.innerHTML == "DEL"){
+            string = string.substring(0,string.length -1);
+            input.value = string
+        }else if(btn.innerHTML == "="){
+            if (string == "") {
+                return
+            }else{
+                if (string[string.length-1] == "%") {
+                    string = +string.substring(0,string.length-1) / 100
+                    input.value = string
+                }else{
+                    string = eval(input.value);
+                    input.value = string
+                }
+            }
+        }else{
+            string = string + btn.innerHTML
+            input.value = string
+        }
+    })
+})
---
+++ readme.md
@@ -0,0 +1,4 @@
+## Calculator <a href="#">video</a>
+Created By <a href="#">Barmajli</a>
+
+<img src="assets/preview.png"/>
